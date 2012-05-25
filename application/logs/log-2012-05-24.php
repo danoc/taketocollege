9c18937b3c5dc6eaeb9cd96e21f4a427db12de66
daniel@danoc.me <daniel@danoc.me>
2012-05-24 23:26:23 -0400

    fixing ignored files

--- application/logs/log-2012-05-24.php
+++
@@ -1,4 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2012-05-24 00:09:22 --> Severity: Notice  --> Trying to get property of non-object /Applications/MAMP/htdocs/taketocollege/application/models/users.php 45
-ERROR - 2012-05-24 00:54:17 --> 404 Page Not Found --> about
