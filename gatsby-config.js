22b28bda8a2a03d8251711c3bf0127b0eb5f8d5b
Daniel O'Connor <daniel@danoc.me>
2018-01-14 15:08:40 -0800

    Create a sitemap on build.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,37 @@
+const regexExclude404 = /^(?!\/(dev-404-page|404|offline-plugin-app-shell-fallback)).*$/;
+
 module.exports = {
+  siteMetadata: {
+    siteUrl: "https://taketocollege.com"
+  },
   plugins: [
     "gatsby-plugin-sass",
     "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-sitemap",
+      options: {
+        query: `{
+          site {
+            siteMetadata {
+              siteUrl
+            }
+          }
+          allSitePage(
+            filter: {
+              path: {
+                regex: "${regexExclude404}"
+              }
+            }
+          ) {
+            edges {
+              node {
+                path
+              }
+            }
+          }
+        }`
+      }
+    },
     "gatsby-plugin-lodash",
     "gatsby-plugin-offline",
     {
